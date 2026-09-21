fe38335cb39b0ec0afb1dc46783b4f87df486f9b
Joshua S. Day <haxmeister@hotmail.com>
2026-09-20 19:07:00 -0500

    Add Mojolicious WebSocket launcher

---
+++ benchmarks/websocket/servers/mojo/server.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use v5.36;
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use JSON::PP ();
+
+my $action = shift // '';
+if ($action eq 'info') {
+    print JSON::PP->new->canonical->encode({
+        label => 'Mojolicious WebSocket',
+        default => JSON::PP::true,
+        order => 20,
+    });
+    exit 0;
+}
+if ($action eq 'probe') {
+    system $^X, '-MMojolicious', '-e', '1';
+    exit($? == 0 ? 0 : 1);
+}
+if ($action eq 'prepare' || $action eq 'cleanup') { exit 0 }
+if ($action eq 'version') {
+    exec $^X, '-MMojolicious', '-e', 'print $Mojolicious::VERSION';
+    exit 127;
+}
+if ($action eq 'settings') {
+    print 'daemon=single-process, permessage-deflate=off';
+    exit 0;
+}
+if ($action eq 'run') {
+    exec $^X, "$Bin/mojo-websocket.pl";
+    die "exec mojo-websocket.pl: $!\n";
+}
+die "usage: server.pl info|probe|prepare|version|settings|run|cleanup\n";
