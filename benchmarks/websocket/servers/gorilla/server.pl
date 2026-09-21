84815bf9f3ce7d2bcd165012f70e192e750d532e
Joshua S. Day <haxmeister@hotmail.com>
2026-09-20 19:07:11 -0500

    Add Gorilla WebSocket launcher

---
+++ benchmarks/websocket/servers/gorilla/server.pl
@@ -0,0 +1,48 @@
+use Cwd ();\n#!/usr/bin/env perl
+use v5.36;
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use JSON::PP ();
+
+my $action = shift // '';
+my $binary = '/tmp/benchmark-web-websocket-gorilla-' . getppid();
+
+if ($action eq 'info') {
+    print JSON::PP->new->canonical->encode({
+        label => 'Go gorilla/websocket',
+        default => JSON::PP::true,
+        order => 40,
+    });
+    exit 0;
+}
+if ($action eq 'probe') {
+    system 'go', 'version';
+    exit($? == 0 ? 0 : 1);
+}
+if ($action eq 'prepare') {
+    my $old = Cwd::getcwd();
+    chdir $Bin or die "chdir $Bin: $!\n";
+    system 'go', 'build', '-o', $binary, '.';
+    my $status = $?;
+    chdir $old or die "chdir $old: $!\n";
+    exit($status == 0 ? 0 : 1);
+}
+if ($action eq 'cleanup') {
+    unlink $binary if -e $binary;
+    exit 0;
+}
+if ($action eq 'version') {
+    exec 'go', 'version';
+    exit 127;
+}
+if ($action eq 'settings') {
+    print 'GOMAXPROCS=1, gorilla/websocket=1.5.3';
+    exit 0;
+}
+if ($action eq 'run') {
+    $ENV{GOMAXPROCS} = 1;
+    exec $binary;
+    die "exec $binary: $!\n";
+}
+die "usage: server.pl info|probe|prepare|version|settings|run|cleanup\n";
